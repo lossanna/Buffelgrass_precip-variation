9e48e8c33d8b709a79978c595e07f2eb91066fd3
Lia Ossanna <lossanna@arizona.edu>
2025-02-28 16:26:10 -0700

    Make more figures to explore GLMs and the effect of aspect

--- scripts/05_draft-figs.R
+++ scripts/05_draft-figs.R
@@ -9,6 +9,7 @@
 
 library(tidyverse)
 library(scales)
+library(viridis)
 
 # Load data ---------------------------------------------------------------
 
@@ -172,7 +173,73 @@ plot.avg.site.aspect %>%
 
 # Reproductive culms ------------------------------------------------------
 
+## Repro: All combined ----------------------------------------------------
+
+# Repro: Linear regression by Perc_dev
+repro.all.lm <- dat %>% 
+  ggplot(aes(x = Perc_dev, y = Reproductive_culms)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_bw() +
+  scale_x_continuous(labels = percent) +
+  geom_vline(xintercept = 0,
+             linetype = "dashed",
+             color = "red") +
+  xlab("Precip deviation from average") +
+  ylab("No. of reproductive culms")
+repro.all.lm
+
+# Repro: geom_smooth() by Perc_dev
+dat %>% 
+  ggplot(aes(x = Perc_dev, y = Reproductive_culms)) +
+  geom_point() +
+  geom_smooth() +
+  theme_bw() +
+  scale_x_continuous(labels = percent) +
+  geom_vline(xintercept = 0,
+             linetype = "dashed",
+             color = "red")  +
+  xlab("Precip deviation from average") +
+  ylab("No. of reproductive culms")
+
+# Repro: Wetter/drier divided, linear regression by Perc_dev
+dat %>% 
+  filter(Perc_dev > 0) %>% 
+  ggplot(aes(x = Perc_dev, y = Reproductive_culms)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_bw() +
+  scale_x_continuous(labels = percent) +
+  ggtitle("Wetter conditions") +
+  xlab("Precip deviation from average") +
+  ylab("No. of reproductive culms")
+dat %>% 
+  filter(Perc_dev < 0) %>% 
+  ggplot(aes(x = Perc_dev, y = Reproductive_culms)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_bw() +
+  scale_x_continuous(labels = percent) +
+  ggtitle("Drier conditions") +
+  xlab("Precip deviation from average") +
+  ylab("No. of reproductive culms")
+
+
+# Repro: Plot slope and Perc_dev
+dat %>% 
+  ggplot(aes(x = PlotSlope, y = Reproductive_culms, color = Perc_dev)) +
+  geom_point() +
+  theme_bw() +
+  scale_color_viridis(option = "viridis", direction = -1)
+
+
 ## Repro: By aspect -------------------------------------------------------
+
+# Repro: Aspect, all conditions (boxplot)
+dat %>% 
+  ggplot(aes(x = Aspect, y = Reproductive_culms)) +
+  geom_boxplot() +
+  theme_bw()
 
 # Repro: Aspect by Year (boxplot, all obs)
 dat %>% 
@@ -189,7 +256,20 @@ dat %>%
   scale_x_continuous(labels = scales::percent) +
   geom_vline(xintercept = 0, linetype = "dashed") +
   facet_wrap(~Aspect) +
-  theme_bw()
+  theme_bw() 
+
+# Repro: Aspect by Perc_dev (linear regression, all obs)
+repro.all.aspect.lm <- dat %>% 
+  ggplot(aes(x = Perc_dev, y = Reproductive_culms)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  scale_x_continuous(labels = scales::percent) +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  facet_wrap(~Aspect) +
+  theme_bw() +
+  xlab("Precip deviation from average") +
+  ylab("No. of reproductive culms")
+repro.all.aspect.lm
 
 # Repro: Aspect by Year (line graph, average)
 culm.avg.aspect %>% 
@@ -200,8 +280,8 @@ culm.avg.aspect %>%
   facet_wrap(~Aspect)
 
 # Repro: Aspect by Perc_dev (line graph, average)
-repro.aspect <- culm.avg.aspect %>% 
-  ggplot(aes(x = Perc_dev_avg, y = repro_avg, color = Aspect)) +
+repro.aspect.avg <- culm.avg.aspect %>% 
+  ggplot(aes(x = Perc_dev_avg, y = repro_avg)) +
   geom_point() +
   geom_line() +
   geom_errorbar(aes(ymin = repro_avg - repro_se, ymax = repro_avg + repro_se)) +
@@ -210,20 +290,9 @@ repro.aspect <- culm.avg.aspect %>%
   scale_x_continuous(labels = scales::percent) +
   xlab("Precip deviation from average") +
   ylab("No. of reproductive culms") +
-  facet_wrap(~Aspect)
-repro.aspect
-
-# Repro: Aspect by Perc_dev (linear regression)
-dat %>% 
-  ggplot(aes(x = Perc_dev, y = Reproductive_culms)) +
-  geom_point() + 
-  geom_smooth(method = "lm") +
-  theme_bw() +
-  scale_x_continuous(labels = scales::percent) +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  xlab("Precip deviation from average") +
-  ylab("No. of reproductive culms") +
-  facet_wrap(~Aspect)
+  facet_wrap(~Aspect) +
+  theme(legend.position = "none")
+repro.aspect.avg
 
 
 
@@ -237,7 +306,7 @@ culm.avg.site %>%
   theme_bw()
 
 # Repro: Site by Perc_dev (line graph, average)
-repro.site <- culm.avg.site %>% 
+repro.site.avg <- culm.avg.site %>% 
   ggplot(aes(x = Perc_dev_avg, y = repro_avg, color = Site)) +
   geom_point() +
   geom_line() +
@@ -247,7 +316,7 @@ repro.site <- culm.avg.site %>%
   scale_x_continuous(labels = scales::percent) +
   xlab("Precip deviation from average") +
   ylab("No. of reproductive culms")
-repro.site
+repro.site.avg
 
 
 ## Repro: By site and aspect ----------------------------------------------
@@ -279,6 +348,65 @@ culm.avg.site.aspect %>%
 
 # Total culms -------------------------------------------------------------
 
+## Total: All combined ----------------------------------------------------
+
+# Total: Linear regression by Perc_dev
+total.all.lm <- dat %>% 
+  ggplot(aes(x = Perc_dev, y = Total_Live_Culms)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_bw() +
+  scale_x_continuous(labels = percent) +
+  geom_vline(xintercept = 0,
+             linetype = "dashed",
+             color = "red") +
+  xlab("Precip deviation from average") +
+  ylab("Total number of culms")
+total.all.lm
+
+# Total: geom_smooth() by Perc_dev
+dat %>% 
+  ggplot(aes(x = Perc_dev, y = Total_Live_Culms)) +
+  geom_point() +
+  geom_smooth() +
+  theme_bw() +
+  scale_x_continuous(labels = percent) +
+  geom_vline(xintercept = 0,
+             linetype = "dashed",
+             color = "red")  +
+  xlab("Precip deviation from average") +
+  ylab("Total number of culms")
+
+# Total: Wetter/drier divided, linear regression by Perc_dev
+dat %>% 
+  filter(Perc_dev > 0) %>% 
+  ggplot(aes(x = Perc_dev, y = Total_Live_Culms)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_bw() +
+  scale_x_continuous(labels = percent) +
+  ggtitle("Wetter conditions") +
+  xlab("Precip deviation from average") +
+  ylab("Total number of culms")
+dat %>% 
+  filter(Perc_dev < 0) %>% 
+  ggplot(aes(x = Perc_dev, y = Total_Live_Culms)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_bw() +
+  scale_x_continuous(labels = percent) +
+  ggtitle("Drier conditions") +
+  xlab("Precip deviation from average") +
+  ylab("Total number of culms")
+
+
+# Total: Plot slope and Perc_dev
+dat %>% 
+  ggplot(aes(x = PlotSlope, y = Total_Live_Culms, color = Perc_dev)) +
+  geom_point() +
+  scale_color_viridis(option = "viridis", direction = -1)
+
+
 ## Total: By aspect -------------------------------------------------------
 
 # Total: Aspect by Year (boxplot, all obs)
@@ -298,6 +426,19 @@ dat %>%
   facet_wrap(~Aspect) +
   theme_bw()
 
+# Repro: Aspect by Perc_dev (linear regression, all obs)
+total.all.aspect.lm <- dat %>% 
+  ggplot(aes(x = Perc_dev, y = Total_Live_Culms)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  scale_x_continuous(labels = scales::percent) +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  facet_wrap(~Aspect) +
+  theme_bw() +
+  xlab("Precip deviation from average") +
+  ylab("Total number of culms")
+total.all.aspect.lm
+
 # Total: Aspect by Year (line graph, average)
 culm.avg.aspect %>% 
   ggplot(aes(x = Year, y = repro_avg, color = Aspect)) +
@@ -307,8 +448,8 @@ culm.avg.aspect %>%
   facet_wrap(~Aspect)
 
 # Total: Aspect by Perc_dev (line graph, average)
-total.aspect <- culm.avg.aspect %>% 
-  ggplot(aes(x = Perc_dev_avg, y = total_avg, color = Aspect)) +
+total.aspect.avg <- culm.avg.aspect %>% 
+  ggplot(aes(x = Perc_dev_avg, y = total_avg)) +
   geom_point() +
   geom_line() +
   geom_errorbar(aes(ymin = total_avg - total_se, ymax = total_avg + total_se)) +
@@ -318,19 +459,7 @@ total.aspect <- culm.avg.aspect %>%
   xlab("Precip deviation from average") +
   ylab("Total number of culms") +
   facet_wrap(~Aspect)
-total.aspect
-
-# Total: Aspect by Perc_dev (linear regression)
-dat %>% 
-  ggplot(aes(x = Perc_dev, y = Total_Live_Culms, color = Aspect)) +
-  geom_point() + 
-  geom_smooth(method = "lm") +
-  theme_bw() +
-  scale_x_continuous(labels = scales::percent) +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  xlab("Precip deviation from average") +
-  ylab("Total number of culms") +
-  facet_wrap(~Aspect)
+total.aspect.avg
 
 
 ## Total: By site ---------------------------------------------------------
@@ -343,7 +472,7 @@ culm.avg.site %>%
   theme_bw()
 
 # Total: Site by Perc_dev (line graph, average)
-total.site <- culm.avg.site %>% 
+total.site.avg <- culm.avg.site %>% 
   ggplot(aes(x = Perc_dev_avg, y = total_avg, color = Site)) +
   geom_point() +
   geom_line() +
@@ -353,7 +482,7 @@ total.site <- culm.avg.site %>%
   scale_x_continuous(labels = scales::percent) +
   xlab("Precip deviation from average") +
   ylab("Total number of culms")
-total.site
+total.site.avg
 
 
 
@@ -384,6 +513,11 @@ culm.avg.site.aspect %>%
 
 # Buffelgrass density -----------------------------------------------------
 
+## BG density: All combined -----------------------------------------------
+
+
+
+
 ## BG density: By aspect --------------------------------------------------
 
 # BG density: Aspect by Year (boxplot, all obs)
@@ -403,6 +537,20 @@ dat.plot %>%
   facet_wrap(~Aspect) +
   theme_bw()
 
+# BG density: Aspect by Perc_dev (linear regression)
+bgden.all.aspect.lm <- dat.plot %>% 
+  ggplot(aes(x = Perc_dev, y = BGDensity)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  scale_x_continuous(labels = scales::percent) +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  facet_wrap(~Aspect) +
+  theme_bw() +
+  labs(x = "Precip deviation from average",
+       y = expression(paste("Density (individuals /  ", m^2, ")")),
+       title = "Buffelgrass density")
+bgden.all.aspect.lm
+
 # BG density: Aspect by Year (line graph, average)
 plot.avg.aspect %>% 
   ggplot(aes(x = Year, y = bgden_avg, color = Aspect)) +
@@ -412,8 +560,8 @@ plot.avg.aspect %>%
   facet_wrap(~Aspect)
 
 # BG density: Aspect by Perc_dev (line graph, average)
-plot.avg.aspect %>% 
-  ggplot(aes(x = Perc_dev_avg, y = bgden_avg, color = Aspect)) +
+bgden.aspect.avg <- plot.avg.aspect %>% 
+  ggplot(aes(x = Perc_dev_avg, y = bgden_avg)) +
   geom_point() +
   geom_line() +
   geom_errorbar(aes(ymin = bgden_avg - bgden_se, ymax = bgden_avg + bgden_se)) +
@@ -422,18 +570,10 @@ plot.avg.aspect %>%
   scale_x_continuous(labels = scales::percent) +
   labs(x = "Precip deviation from average",
        y = expression(paste("Density (individuals /  ", m^2, ")"))) +
-  facet_wrap(~Aspect) +
-  theme(legend.position = "none")
+  facet_wrap(~Aspect) 
+bgden.aspect.avg
 
-# BG density: Aspect by Perc_dev (linear regression)
-dat.plot %>% 
-  ggplot(aes(x = Perc_dev, y = BGDensity)) +
-  geom_point() +
-  geom_smooth(method = "lm") +
-  scale_x_continuous(labels = scales::percent) +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  facet_wrap(~Aspect) +
-  theme_bw()
+
 
 
 ## BG density: By site ----------------------------------------------------
@@ -446,7 +586,7 @@ plot.avg.site %>%
   theme_bw()
 
 # BG density: Site by Perc_dev (line graph, average)
-bgden.site <- plot.avg.site %>% 
+bgden.site.avg <- plot.avg.site %>% 
   ggplot(aes(x = Perc_dev_avg, y = bgden_avg, color = Site)) +
   geom_point() +
   geom_line() +
@@ -455,8 +595,9 @@ bgden.site <- plot.avg.site %>%
   theme_bw() +
   scale_x_continuous(labels = scales::percent) +
   labs(x = "Precip deviation from average",
-       y = expression(paste("Density (individuals /  ", m^2, ")"))) 
-bgden.site
+       y = expression(paste("Density (individuals /  ", m^2, ")")),
+       title = "Buffelgrass density") 
+bgden.site.avg
 
 # BG density: Site by Perc_dev (linear regression)
 dat.plot %>% 
@@ -490,7 +631,8 @@ plot.avg.site.aspect %>%
   facet_wrap(~Site) +
   scale_x_continuous(labels = scales::percent) +
   labs(x = "Precip deviation from average",
-       y = expression(paste("Density (individuals /  ", m^2, ")"))) 
+       y = expression(paste("Density (individuals /  ", m^2, ")")),
+       title = "Buffelgrass density") 
 
 
 
@@ -514,6 +656,20 @@ dat.plot %>%
   geom_vline(xintercept = 0, linetype = "dashed") +
   facet_wrap(~Aspect) +
   theme_bw() 
+
+# BG cover: Aspect by Perc_dev (linear regression)
+bgcov.all.aspect.lm <- dat.plot %>% 
+  ggplot(aes(x = Perc_dev, y = BGCover)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  scale_x_continuous(labels = scales::percent) +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  facet_wrap(~Aspect) +
+  theme_bw() +
+  labs(x = "Precip deviation from average",
+       y = "Cover (%)",
+       title = "Buffelgrass cover")
+bgcov.all.aspect.lm
 
 # BG cover: Aspect by Year (line graph, average)
 plot.avg.aspect %>% 
@@ -559,7 +715,7 @@ plot.avg.site %>%
   theme_bw()
 
 # BG cover: Site by Perc_dev (line graph, average)
-bgcov.site <- plot.avg.site %>% 
+bgcov.site.avg <- plot.avg.site %>% 
   ggplot(aes(x = Perc_dev_avg, y = bgcov_avg, color = Site)) +
   geom_point() +
   geom_line() +
@@ -569,7 +725,7 @@ bgcov.site <- plot.avg.site %>%
   scale_x_continuous(labels = scales::percent) +
   xlab("Precip deviation from average") +
   ylab("Buffelgrass cover (%)")
-bgcov.site
+bgcov.site.avg
 
 
 
@@ -718,6 +874,8 @@ plot.avg.site.aspect %>%
 
 # Write out draft figures -------------------------------------------------
 
+## Precipitation deviation ------------------------------------------------
+
 tiff("figures/2025-02_draft-figures/Precip-deviation_site.tiff", units = "in", height = 5, width = 7, res = 150)
 precip.dev.site
 dev.off()
@@ -726,20 +884,89 @@ tiff("figures/2025-02_draft-figures/Precip-deviation_aspect.tiff", units = "in",
 precip.dev.aspect
 dev.off()
 
-tiff("figures/2025-02_draft-figures/Reproductive-culms_precip-dev-by-site.tiff", units = "in", height = 5, width = 6, res = 150)
-repro.site
+
+## Reproductive culms -----------------------------------------------------
+
+# All observations
+tiff("figures/2025-02_draft-figures/Reproductive-culms_precip-dev_all_regression.tiff", 
+     units = "in", height = 4, width = 6, res = 150)
+repro.all.lm
 dev.off()
 
-tiff("figures/2025-02_draft-figures/Total-culms_precip-dev-by-site.tiff", units = "in", height = 5, width = 6, res = 150)
-total.site
+tiff("figures/2025-02_draft-figures/Reproductive-culms_precip-dev-by-aspect_all_regression.tiff", 
+     units = "in", height = 4, width = 6, res = 150)
+repro.all.aspect.lm
 dev.off()
 
-tiff("figures/2025-02_draft-figures/BG-density_precip-dev-by-site.tiff", units = "in", height = 5, width = 6, res = 150)
-bgden.site
+# Averages
+tiff("figures/2025-02_draft-figures/Reproductive-culms_precip-dev-by-aspect_avg.tiff", 
+     units = "in", height = 4, width = 6, res = 150)
+repro.aspect.avg
 dev.off()
 
-tiff("figures/2025-02_draft-figures/BG-cover_precip-dev-by-site.tiff", units = "in", height = 5, width = 6, res = 150)
-bgcov.site
+tiff("figures/2025-02_draft-figures/Reproductive-culms_precip-dev-by-site_avg.tiff", 
+     units = "in", height = 5, width = 6, res = 150)
+repro.site.avg
+dev.off()
+
+
+## Total culms ------------------------------------------------------------
+
+# All observations
+tiff("figures/2025-02_draft-figures/Total-culms_precip-dev_all_regression.tiff", 
+     units = "in", height = 4, width = 6, res = 150)
+total.all.lm
+dev.off()
+
+tiff("figures/2025-02_draft-figures/Total-culms_precip-dev-by-aspect_all_regression.tiff", 
+     units = "in", height = 4, width = 6, res = 150)
+total.all.aspect.lm
+dev.off()
+
+# Averages
+tiff("figures/2025-02_draft-figures/Total-culms_precip-dev-by-aspect_avg.tiff", 
+     units = "in", height = 4, width = 6, res = 150)
+total.aspect.avg
+dev.off()
+
+tiff("figures/2025-02_draft-figures/Total-culms_precip-dev-by-site_avg.tiff", units = "in", height = 5, width = 6, res = 150)
+total.site.avg
+dev.off()
+
+
+
+## Buffelgrass density ----------------------------------------------------
+
+# All observations
+tiff("figures/2025-02_draft-figures/BG-density_precip-dev-by-aspect_all_regression.tiff", 
+     units = "in", height = 4, width = 6, res = 150)
+bgden.all.aspect.lm
+dev.off()
+
+# Averages
+tiff("figures/2025-02_draft-figures/BG-density_precip-dev-by-aspect_avg.tiff", 
+     units = "in", height = 4, width = 6, res = 150)
+bgden.aspect.avg
+dev.off()
+
+tiff("figures/2025-02_draft-figures/BG-density_precip-dev-by-site_avg.tiff", 
+     units = "in", height = 5, width = 6, res = 150)
+bgden.site.avg
+dev.off()
+
+
+## Buffelgrass cover ------------------------------------------------------
+
+# All observations
+tiff("figures/2025-02_draft-figures/BG-cover_precip-dev-by-aspect_all_regression.tiff", 
+     units = "in", height = 4, width = 6, res = 150)
+bgcov.all.aspect.lm
+dev.off()
+
+# Averages
+tiff("figures/2025-02_draft-figures/BG-cover_precip-dev-by-site_avg.tiff", 
+     units = "in", height = 5, width = 6, res = 150)
+bgcov.site.avg
 dev.off()
 
 
