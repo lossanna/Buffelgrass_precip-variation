6524ddffd13ec2451ba22e2f2fcd4bfdf7cb656e
Lia Ossanna <lossanna@arizona.edu>
2025-08-01 09:47:49 -0600

    Begin script to compile data for publishing

---
+++ scripts/07_data-for-publishing.R
@@ -0,0 +1,34 @@
+# Created: 2025-08-01
+# Updated: 2025-08-01
+
+# Purpose: Create data for publishing.
+
+library(tidyverse)
+
+# Load data ---------------------------------------------------------------
+
+dat.raw <- read_csv("data/cleaned/04_demography-data_clean.csv")
+culm.change.raw <- read_csv("data/cleaned/04_change-in-culm-density-cover_clean.csv")
+prism.dat.raw <- read_csv("data/cleaned/02_monitoring-info-with-PRISM-data_clean.csv")
+
+# Data wrangling ----------------------------------------------------------
+
+# Site info
+prism.dat <- prism.dat.raw %>% 
+  select(Site, Latitude, Longitude, Elevation_ft, MAP, MAT) %>% 
+  distinct(.keep_all = TRUE)
+
+# Culm change
+culm.change <- culm.change.raw %>% 
+  select(Year, Site, Transect, Plot, Plant_ID, Aspect, PlotSlope, Prev_year_precip,
+         Change_Reproductive_culms, Change_Total_Live_Culms, Change_BGDensity, Change_BGCover,
+         Change_ShrubCover, Change_HerbCover) %>% 
+  filter(Aspect != "flat") %>% 
+  rename(Change_ReproductiveCulms = Change_Reproductive_culms,
+         Change_TotalCulms = Change_Total_Live_Culms)
+
+# Plot change
+plot.change <- culm.change %>% 
+  select(Year, Site, Transect, Plot, Aspect, PlotSlope, Prev_year_precip,
+         Change_BGDensity, Change_BGCover, Change_ShrubCover, Change_HerbCover) %>% 
+  distinct(.keep_all = TRUE) 
